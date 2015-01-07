6cdd1f699ccb5fc605c6675a7c9e76bed194df68
xiaoyaojian <xiaoyaojian@126.com>
2015-01-07 20:20:57 +0800

    add disabled property

--- ListControl.Extension/ListControlExtensions.cs
+++ ListControl.Extension/ListControlExtensions.cs
@@ -11,28 +11,28 @@ namespace ListControl.Extension
 {
     public static class ListControlExtensions
     {
-        public static MvcHtmlString RadioButtonList(this HtmlHelper htmlHelper, string name, List<SelectListItem> items, string cssClass=null)
+        public static MvcHtmlString RadioButtonList(this HtmlHelper htmlHelper, string name, List<SelectListItem> items, string cssClass=null,bool disabled=false)
         {
-            return ListControlUtil.GenerateHtml(name, items, ControlType.Radio, null,cssClass);
+            return ListControlUtil.GenerateHtml(name, items, ControlType.Radio, null, cssClass, disabled);
         }
-        public static MvcHtmlString RadioButtonListFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, List<SelectListItem> items, string cssClass=null)
+        public static MvcHtmlString RadioButtonListFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, List<SelectListItem> items, string cssClass = null, bool disabled = false)
         {
             ModelMetadata metadata = ModelMetadata.FromLambdaExpression<TModel, TProperty>(expression, htmlHelper.ViewData);
             string name = ExpressionHelper.GetExpressionText(expression);
             string fullHtmlFieldName = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(name);
-            return ListControlUtil.GenerateHtml(fullHtmlFieldName, items, ControlType.Radio, metadata.Model, cssClass);
+            return ListControlUtil.GenerateHtml(fullHtmlFieldName, items, ControlType.Radio, metadata.Model, cssClass, disabled);
         }
 
-        public static MvcHtmlString CheckBoxList(this HtmlHelper htmlHelper, string name, List<SelectListItem> items,  string cssClass = null)
+        public static MvcHtmlString CheckBoxList(this HtmlHelper htmlHelper, string name, List<SelectListItem> items, string cssClass = null, bool disabled = false)
         {
-            return ListControlUtil.GenerateHtml(name, items, ControlType.CheckBox, null, cssClass);
+            return ListControlUtil.GenerateHtml(name, items, ControlType.CheckBox, null, cssClass, disabled);
         }
-        public static MvcHtmlString CheckBoxListFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, List<SelectListItem> items,  string cssClass = null)
+        public static MvcHtmlString CheckBoxListFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, List<SelectListItem> items, string cssClass = null, bool disabled = false)
         {
             ModelMetadata metadata = ModelMetadata.FromLambdaExpression<TModel, TProperty>(expression, htmlHelper.ViewData);
             string name = ExpressionHelper.GetExpressionText(expression);
             string fullHtmlFieldName = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(name);
-            return ListControlUtil.GenerateHtml(fullHtmlFieldName, items, ControlType.CheckBox, metadata.Model, cssClass);
+            return ListControlUtil.GenerateHtml(fullHtmlFieldName, items, ControlType.CheckBox, metadata.Model, cssClass, disabled);
         }
     }
 }
--- ListControl.Extension/ListControlUtil.cs
+++ ListControl.Extension/ListControlUtil.cs
@@ -11,7 +11,7 @@ namespace ListControl.Extension
 {
     internal static class ListControlUtil
     {
-        public static MvcHtmlString GenerateHtml(string name, List<SelectListItem> items, ControlType controlType, object stateValue,string cssClass)
+        public static MvcHtmlString GenerateHtml(string name, List<SelectListItem> items, ControlType controlType, object stateValue,string cssClass,bool disabled)
         {
             TagBuilder container = new TagBuilder("div");
             container.AddCssClass(cssClass);
@@ -20,45 +20,45 @@ namespace ListControl.Extension
             {
                 case ControlType.CheckBox:
                     IEnumerable<string> currentValues = stateValue as IEnumerable<string>;
-                    result= GenerateCheckBoxList(container, name, items, currentValues);
+                    result= GenerateCheckBoxList(container, name, items, currentValues,disabled);
                     break;
                 case ControlType.Radio:
                     string currentValue = stateValue as string;
-                    result= GenerateRadioButtonList(container, name, items, currentValue);
+                    result= GenerateRadioButtonList(container, name, items, currentValue,disabled);
                     break;
             }
             return result;
         }
 
         private static MvcHtmlString GenerateCheckBoxList(TagBuilder container, string name, IEnumerable<SelectListItem> items,
-            IEnumerable<string> currentValues)
+            IEnumerable<string> currentValues, bool disabled)
         {
             int idSuffix = 1;
             foreach (var item in items)
             {
                 string controlId = string.Format("{0}{1}", name, idSuffix);
                 bool currentItemIschecked = currentValues != null ? currentValues.Any(i => i == item.Value) : false;
-                container.InnerHtml += GenerateControl(name, controlId, item.Text, item.Value, currentItemIschecked,"checkbox");
+                container.InnerHtml += GenerateControl(name, controlId, item.Text, item.Value, currentItemIschecked,"checkbox", disabled);
                 idSuffix++;
             }
             return new MvcHtmlString(container.ToString());
         }
 
         private static MvcHtmlString GenerateRadioButtonList(TagBuilder container, string name, IEnumerable<SelectListItem> items,
-            string currentValue)
+            string currentValue, bool disabled)
         {
             int idSuffix = 1;
             foreach (var item in items)
             {
                 string controlId = string.Format("{0}{1}", name, idSuffix);
                 bool currentItemIschecked = item.Value == currentValue;
-                container.InnerHtml += GenerateControl(name, controlId, item.Text, item.Value, currentItemIschecked, "radio");
+                container.InnerHtml += GenerateControl(name, controlId, item.Text, item.Value, currentItemIschecked, "radio", disabled);
                 idSuffix++;
             }
             return new MvcHtmlString(container.ToString());
         }
 
-        private static string GenerateControl(string name, string id, string labelText, string value, bool isChecked, string type)
+        private static string GenerateControl(string name, string id, string labelText, string value, bool isChecked, string type,bool disabled)
         {
             StringBuilder sb = new StringBuilder();
             TagBuilder label = new TagBuilder("label");
@@ -69,6 +69,10 @@ namespace ListControl.Extension
             input.MergeAttribute("name", name);
             input.MergeAttribute("type", type);
             input.MergeAttribute("value", value);
+            if (disabled)
+            {
+                input.MergeAttribute("disabled", "disabledw");
+            }
             if (isChecked)
             {
                 input.MergeAttribute("checked", "checked");
