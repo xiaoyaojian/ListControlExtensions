1087429ae73cb78bf42b59f31755ee9d912307a5
Zhu Ming <mingzhu.z@gmail.com>
2015-04-03 20:51:38 +0800

    add Employee/Edit action to show the load problem

--- ListControl.Extension.Example/Controllers/EmployeeController.cs
+++ ListControl.Extension.Example/Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,6 +19,41 @@ namespace ListControl.Extension.Example.Controllers
         public ActionResult Create(Employee employee)
         {
             throw new NotImplementedException();
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = new Employee()
+            {
+                Name = "Temp",
+                Departments = new List<int> { 1, 2 },
+                Gender = 1
+            };
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Name,Departments,Gender")] 
+            Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(employee).State = EntityState.Modified;
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
         }
     }
 }
